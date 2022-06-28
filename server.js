001d93d27a71a8d2ab84e5f41fba5dcb5e2183ea
Bekir - CDI <b.burakcebecii@gmail.com>
2022-06-28 15:16:09 +0200

    cors in use..

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
 const port = 3054;
+
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("<h1>Getting Data From SQL or NoSQL Database</h1>");
