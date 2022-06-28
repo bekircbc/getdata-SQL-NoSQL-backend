8e4423c631a490f7a76dbfc8386b4bf0cc87046c
Bekir - CDI <b.burakcebecii@gmail.com>
2022-06-28 15:25:12 +0200

    main.css added to page..

--- server.js
+++ server.js
@@ -9,7 +9,19 @@ app.use(cors());
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  res.send("<h1>Getting Data From SQL or NoSQL Database</h1>");
+  res.send(`<!DOCTYPE html>
+  <html lang="en">
+  <head>
+      <meta charset="UTF-8" />
+      <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <link rel="stylesheet" href="main.css" />
+      <title>SQL vs. NoSQL</title>
+  </head>
+  <body>
+  <h1>Getting Data From SQL or NoSQL Database</h1>
+  </body>
+  </html>`);
 });
 
 app.listen(port, () => {
