319c6eecc7a44f8678f9cc9232e29a7114125f31
Bekir - CDI <b.burakcebecii@gmail.com>
2022-06-28 15:13:39 +0200

    added link with h1 tag

--- server.js
+++ server.js
@@ -2,6 +2,11 @@ import express from "express";
 
 const app = express();
 const port = 3054;
+
+app.get("/", (req, res) => {
+  res.send("<h1>Getting Data From SQL or NoSQL Database</h1>");
+});
+
 app.listen(port, () => {
   console.log(`listening on port:http://localhost:${port}`);
 });
